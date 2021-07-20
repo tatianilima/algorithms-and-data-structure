00cb459d0850ab398c912519ed182e05aac87896
Tatiani de Andrade <tatianilima@hotmail.com>
2021-07-20 11:53:34 -0300

    chore: add new methods

--- hashtable/Program.cs
+++ hashtable/Program.cs
@@ -7,31 +7,74 @@ namespace hashtable
     {
         static void Main(string[] args)
         {
+            Hashtable ht = GetHashtable();
+
+            //Check if Hashtable contains key or value
+            CheckHashtable(ht);
+
+            //Show keys/values from Hashtable
+            ShowOnlyKeys(ht);
+            ShowValuesByKey(ht);
+
+            //Remove elements from Hashtable
+            RemoveElement(ht);
+
+            //remove all elements
+            RemoveAllElements(ht);
+
+            Console.ReadLine();
+        }
+
+        static Hashtable GetHashtable()
+        {
             Hashtable ht = new Hashtable();
-            
+
             //How to add items on hashtable. Example: ht.Add("key", "value");
             ht.Add("0001", "C#");
             ht.Add("0002", "Java");
             ht.Add("0003", "Golang");
             ht.Add("0004", "Python");
 
+            return ht;
+        }
+
+        static void CheckHashtable(Hashtable ht)
+        {
+            Console.WriteLine(ht.ContainsKey("0001"));
+            Console.WriteLine(ht.ContainsValue("C#"));
+            Console.WriteLine(ht.ContainsValue("PHP"));
+            Console.WriteLine(ht.Contains("Java"));
+        }
+
+        static void ShowOnlyKeys(Hashtable ht)
+        {
+            
+            foreach (int key in ht.Keys)
+            {
+                Console.WriteLine("Keys : " + key);
+            }
+        }
+
+        static void ShowValuesByKey(Hashtable ht)
+        {
             ICollection keys = ht.Keys;
             foreach (String k in keys)
             {
                 Console.WriteLine(ht[k]);
             }
 
-            //Searching some word 
-            Console.WriteLine(ht.ContainsKey("0001"));
-            Console.WriteLine(ht.ContainsValue("C#"));
-            Console.WriteLine(ht.ContainsValue("PHP"));
+        }
 
-            //Remove elements from Hashtable
+        static void RemoveElement(Hashtable ht)
+        {
             ht.Remove("Python");
             Console.WriteLine(ht.ContainsValue("Python"));
-            ht.Clear(); //remove all elements
+        }
 
-            Console.ReadLine();
+        static void RemoveAllElements(Hashtable ht)
+        {
+            ht.Clear();
+            Console.WriteLine(ht.ContainsValue("Python"));
         }
     }
 }
