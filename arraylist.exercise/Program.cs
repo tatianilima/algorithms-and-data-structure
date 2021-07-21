7aedadaabb66980ed2690381d9ae39913589b168
Tatiani de Andrade <tatianilima@hotmail.com>
2021-07-21 13:48:09 -0300

    feat: add array list

---
+++ arraylist.exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace arraylist.exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arlist = new ArrayList()
+                {
+                    2, "Steve", " ", true, 4.5, null
+                };
+            var firstElement = arlist[0];
+            var secondElement = arlist[1];
+            Console.WriteLine(firstElement);
+            Console.WriteLine(secondElement);
+            Console.ReadLine();
+        }
+    }
+}
