95cb74af3e6d3c023a93645d069c7021d887d373
Tatiani de Andrade <tatianilima@hotmail.com>
2021-07-24 11:37:50 -0300

    feat: add binary search tree

---
+++ 8-Binary.Search.Tree/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+/*
+ * Binary Search Tree is a binary tree in wich every node fits a especific ordering property: 
+ * all left descendents <= n < all right descendents. Its must be true for each node n.
+ * Exercise based in https://betterprogramming.pub/introduction-to-binary-search-trees-dde166368210
+ */
+namespace _8_Binary.Search.Tree
+{
+    class Node
+    {
+        public int leaf;
+        Node left;
+        Node right;
+
+        public Node(int data)
+        {
+            leaf = data;
+            left = null;
+            right = null;
+        }
+
+        public void AddNode(Node root)
+        {
+            //root.leaf == leaf -> duplicate values are not allowed
+            if (root == null || root.leaf == leaf)
+                return;
+            //TODO: Add here conditions to add left and right node
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node root = null;
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int x = rnd.Next(0, 100);
+                Node n = new Node(x);
+                if (root == null)
+                {
+                    Console.WriteLine("Setting " + n.leaf + " to the root");
+                    root = n;
+                }
+                else
+                {
+                    n.AddNode(root);
+                }                    
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
