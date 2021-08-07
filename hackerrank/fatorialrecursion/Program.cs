b66ceab6799340b1a4e206997ee8dad65faa33c2
Tatiani de Andrade <tatianilima@hotmail.com>
2021-08-06 22:42:12 -0300

    factorial

---
+++ hackerrank/fatorialrecursion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+/*
+ * Conhecemos como fatorial de um número natural a multiplicação desse número 
+ * por seus antecessores com exceção do zero.
+ */
+namespace fatorialrecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+
+            int result = factorial(n);
+
+            Console.WriteLine(result);
+        }
+
+        public static int factorial(int n)
+        {
+            if (n == 1 || n == 0)
+                return 1;
+            else
+                return n * factorial(n - 1);
+
+        }
+    }
+}
